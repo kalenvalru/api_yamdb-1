97922c2266305592afb6c417d2694f04b7de48e8
Павел Новиков <pnovikov.postbox@gmail.com>
2023-03-25 21:19:49 +0300

    pytest 02/02/04 plus flake8

--- api_yamdb/api/filters.py
+++ api_yamdb/api/filters.py
@@ -1,10 +1,11 @@
 from django_filters.rest_framework import CharFilter, FilterSet
 from reviews.models import Title
 
+
 class FilterTitleSet(FilterSet):
     genre = CharFilter('genre__slug')
     category = CharFilter('category__slug')
-    
+
     class Meta:
         model = Title
         fields = (
